7e4f88eb5788fadf93bf6526c705475c7dea3050
The Analyst <42177776+analyst045@users.noreply.github.com>
2021-08-11 11:06:04 +1000

    Update starter_script.sh

--- cloud-functions/cloud_function_1/starter_script.sh
+++ cloud-functions/cloud_function_1/starter_script.sh
@@ -2,7 +2,7 @@
 # use this to set the project and activate the service account from your local
 # credentials.json would be your service account file
 # don't forget to give relevant access needed to service account 
-gcloud config set project mydeal-bigquery
+gcloud config set project project-name
 gcloud auth activate-service-account \
     service-account.iam.gserviceaccount.com \
     --key-file=credentials.json
